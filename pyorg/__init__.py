8129d6b5644df04117749cea82006d5577bc3a21
Jared Lumpe <mjlumpe@gmail.com>
2019-01-19 21:36:18 -0800

    New package from cookiecutter

---
+++ pyorg/__init__.py
@@ -0,0 +1,8 @@
+"""Root package for pyorg.
+
+Package for woring with Emacs org-mode files
+"""
+
+__author__ = 'Jared Lumpe'
+__email__ = 'mjlumpe@gmail.com'
+__version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,51 @@
+"""Setuptools installation script for pyorg package."""
+
+from setuptools import setup
+import re
+
+
+# Get contents of README file
+with open('README.md') as fh:
+	readme_contents = fh.read()
+
+
+# Read version from root module __init__.py
+with open('pyorg/__init__.py') as fh:
+	init_contents = fh.read()
+	version_match = re.search('^__version__ = ["\']([^"\']+)["\']', init_contents, re.M)
+
+	if not version_match:
+		raise RuntimeError('Unable to get version string')
+
+	version = version_match.group(1)
+
+
+requirements = [
+]
+
+setup_requirements = ['pytest-runner']
+
+test_requirements = ['pytest']
+
+
+setup(
+	name='pyorg',
+	version=version,
+	description='Package for woring with Emacs org-mode files',
+	long_description=readme_contents,
+	author='Jared Lumpe',
+	author_email='mjlumpe@gmail.com',
+	url='https://github.com/jlumpe/pyorg',
+	python_requires='>=3.5',
+	install_requires=requirements,
+	setup_requires=setup_requirements,
+	tests_require=test_requirements,
+	include_package_data=True,
+	# license='',
+	# classifiers='',
+	# keywords=[],
+	# platforms=[],
+	# provides=[],
+	# requires=[],
+	# obsoletes=[],
+)
