f71749e2f8921bb5b373bf596dba321c7aecff9c
Jared Lumpe <mjlumpe@gmail.com>
2019-02-08 01:00:22 -0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const gulp = require('gulp'),
-      path = require('path'),
+const {src, dest, series, parallel, watch} = require('gulp');
+const path = require('path'),
       del = require('del'),
       concat = require('gulp-concat'),
       sass = require('gulp-sass'),
@@ -15,35 +15,37 @@ const static_dir = './pyorg/app/static/';
 const lib_files = [
     'jquery/dist/jquery.js',
     'bootstrap/dist/js/bootstrap.js',
+    'popper.js/dist/popper.js',
+    'bootstrap/dist/js/bootstrap.js',
 ];
 
 function libs_concat() {
-    return gulp.src(lib_files.map(f => path.join('node_modules', f)))
+    return src(lib_files.map(f => path.join('node_modules', f)))
         .pipe(concat('lib/libs.js'))
-        .pipe(gulp.dest(static_dir));
+        .pipe(dest(static_dir));
 }
 
 function libs_mathjax() {
-    return gulp.src('node_modules/mathjax/**/*')
-        .pipe(gulp.dest(path.join(static_dir, 'lib/MathJax')));
+    return src('node_modules/mathjax/**/*')
+        .pipe(dest(path.join(static_dir, 'lib/MathJax')));
 }
 
 
-const libs = gulp.parallel(libs_concat, libs_mathjax);
+const libs = parallel(libs_concat, libs_mathjax);
 
 
 function compile_sass() {
-    return gulp.src('./assets/**/*.scss')
+    return src('./assets/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
             includePaths: ['node_modules'],
         }).on('error', sass.logError))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(static_dir));
+        .pipe(dest(static_dir));
 }
 
 function watch_sass() {
-    gulp.watch('./assets/**/*.scss', ['sass']);
+    watch('./assets/**/*.scss', [compile_sass]);
 }
 
 
@@ -52,11 +54,11 @@ function clean() {
 }
 
 
-const build = gulp.parallel(libs, compile_sass);
+const build = parallel(libs, compile_sass);
 
 
 exports.libs = libs;
 exports.sass = compile_sass;
 exports.build = build;
 exports.clean = clean;
-exports.default = gulp.series(clean, build);
+exports.default = series(clean, build);
