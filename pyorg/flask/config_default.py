16350ecf9e0aa338b43d53229d3b003432ecb619
Jared Lumpe <mjlumpe@gmail.com>
2019-04-23 17:38:26 -0700

    Update default config

--- pyorg/flask/config_default.py
+++ pyorg/flask/config_default.py
@@ -1,3 +1,15 @@
-ORG_DIR = '/Users/student/org/'
+"""Default configuration file for pyorg flask app."""
 
-ORG_FAVORITE_FILES = []
+
+# Root org directory (defaults to value of `org-directory` in Emacs)
+# PYORG_ORG_DIRECTORY = '~/org'
+
+# Base command to run Emacs in non-interactive mode
+# PYORG_EMACS_CMD = ['emacs', '--batch']
+PYORG_EMACS_CMD = ['emacsclient']
+
+# Whether PYORG_EMACS_CMD is running emacsclient
+PYORG_EMACS_IS_CLIENT = True
+
+# List of favorite files (relative to org directory).
+PYORG_FAVORITE_FILES = []
