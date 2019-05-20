0142934e18999689fde6a78125bf9b95feacd5f5
Jared Lumpe <mjlumpe@gmail.com>
2019-05-20 15:41:25 -0700

    Use own HTML element class instead of xml.dom.minidom

--- pyorg/html.py
+++ pyorg/html.py
@@ -1,28 +1,96 @@
 """Export org mode AST nodes to HTML."""
 
 from collections import ChainMap
-from xml.dom.minidom import Document
+from html import escape
+from io import StringIO
 
 from .ast import ORG_ALL_OBJECTS, get_node_type
 
 
-def add_class(elem, class_):
-	"""Add class(es) to an HTML element.
+class HtmlElement:
+	"""Lightweight class to represent an HTML element.
 
-	Parameters
+	Attributes
 	----------
-	elem : xml.dom.minidom.Element
-		Element to add class to.
-	class_: str or list
-		String or list of strings.
+	tag : str
+	children : list
+	attrs : dict
+	inline : bool
+	classes : list
 	"""
-	if not isinstance(class_, str):
-		class_ = ' '.join(class_)
 
-	if 'class' in elem.attributes:
-		elem.attributes['class'] = elem.attributes['class'].value + ' ' + class_
-	else:
-		elem.attributes['class'] = class_
+	def __init__(self, tag, children=None, attrs=None, inline=False):
+		self.tag = tag
+		self.children = list(children or [])
+		self.attrs = dict(attrs or [])
+		self.inline = inline
+
+	@property
+	def classes(self):
+		s = self.attrs.get('class', '').strip()
+		return s.split() if s else []
+
+	@classes.setter
+	def classes(self, value):
+		if not isinstance(value, str):
+			value = ' '.join(value)
+		self.attrs['class'] = value
+
+	def add_class(self, classes):
+		current = self.classes
+
+		if isinstance(classes, str):
+			classes = classes.split()
+
+		for cls in classes:
+			if cls not in current:
+				current.append(cls)
+
+		self.classes = current
+
+	def __repr__(self):
+		return '<' + self.tag + (' ...' if self.attrs else '') + ('>...</' + self.tag if self.children else '/>')
+
+	def __str__(self):
+		return html_to_string(self)
+
+
+def _write_html_recursive(stream, elem, indent, depth, inline=False):
+	inline = inline or elem.inline
+
+	# Opening tag and attrs
+	stream.write('<' + elem.tag)
+
+	for key, value in elem.attrs.items():
+		stream.write(' %s="%s"' % (escape(key), escape(value)))
+
+	stream.write('>')
+
+	for child in elem.children:
+		if not inline:
+			stream.write('\n')
+			stream.write(indent * (depth + 1))
+
+		if isinstance(child, str):
+			stream.write(escape(child))
+		else:
+			_write_html_recursive(stream, child, indent=indent, depth=depth + 1, inline=inline)
+
+	if elem.children and not inline:
+		stream.write('\n')
+		stream.write(indent * depth)
+
+	stream.write('</%s>' % elem.tag)
+
+
+def write_html(stream, elem, indent='\t', inline=False):
+	_write_html_recursive(stream, elem, indent, depth=0, inline=inline)
+
+
+def html_to_string(elem, **kwargs):
+	buf = StringIO()
+	write_html(buf, elem, **kwargs)
+	return buf.getvalue()
 
 
 class DispatchNodeType:
@@ -101,6 +169,11 @@ class OrgHtmlConverter:
 		'statistics-cookie': 'span',
 	}
 
+	INLINE_NODES = frozenset({
+		'paragraph', 'example-block', 'fixed-width',
+		*ORG_ALL_OBJECTS,
+	})
+
 	DEFAULT_CONFIG = {
 		'latex_delims': ('$$', '$$'),
 		'latex_inline_delims': (r'\(', r'\)'),
@@ -108,8 +181,6 @@ class OrgHtmlConverter:
 	}
 
 	def __init__(self, config=None, **kw):
-		self.doc = Document()
-
 		if config is None:
 			config = {}
 		if kw:
@@ -130,23 +201,23 @@ class OrgHtmlConverter:
 		node : pyorg.ast.OrgNode
 			Org node to convert.
 		dom : bool
-			Return XML DOM element instead of string.
+			Return HTML element instead of string.
 
 		Returns
 		-------
-		str or xml.dom.minidom.Element
+		str or HtmlElement
 		"""
 		elem = self._convert(node, None)
 
 		if dom:
 			return elem
 
-		return elem.toprettyxml()
+		return str(elem)
 
 	def _convert(self, what, ctx):
 		"""Convert an org AST node OR text to HTML element."""
 		if isinstance(what, str):
-			return self.doc.createTextNode(what)
+			return what
 		else:
 			return self._convert_node(what, ctx)
 
@@ -158,18 +229,15 @@ class OrgHtmlConverter:
 	_convert_node = dispatch_node_type()(_convert_node_default)
 	_convert_node.__doc__ = """Recursively _convert an org AST node to HTML."""
 
-	def _make_elem_base(self, tag, text=None, attrs=None, classes=None):
+	def _make_elem_base(self, tag, text=None, attrs=None, classes=None, inline=False):
 		"""Create a new HTML element."""
-		html = self.doc.createElement(tag)
+		html = HtmlElement(tag, inline=inline)
 		if text is not None:
-			html.appendChild(self.doc.createTextNode(text))
+			html.children.append(text)
 		if attrs is not None:
-			for key, value in attrs.items():
-				html.attributes[key] = value
+			html.attrs.update(attrs)
 		if classes is not None:
-			if not isinstance(classes, str):
-				classes = ' '.join(classes)
-			html.attributes['class'] = classes
+			html.classes = classes
 		return html
 
 	def _make_elem_default(self, node, ctx, tag=None, **kwargs):
@@ -183,14 +251,16 @@ class OrgHtmlConverter:
 			if tag is None:
 				return None
 
+		kwargs.setdefault('inline', node.type in self.INLINE_NODES)
+
 		html = self._make_elem_base(tag, **kwargs)
-		add_class(html, 'org-node org-%s' % node.type)
+		html.add_class('org-node org-%s' % node.type)
 
 		# Warn about no default tag
 		if no_default:
 			msg = "Don't know how to convert node of type %r" % node.type
 			self._add_error(html)
-			html.appendChild(self._make_error_msg(msg))
+			html.children.append(self._make_error_msg(msg))
 
 		return html
 
@@ -205,7 +275,7 @@ class OrgHtmlConverter:
 		for node in org_nodes:
 			html = self._convert(node, ctx)
 			if html is not None:
-				parent.appendChild(html)
+				parent.children.append(html)
 
 	def make_headline_text(self, node, ctx=None, dom=False):
 		"""Make HTML element for text content of headline node."""
@@ -215,22 +285,22 @@ class OrgHtmlConverter:
 		if dom:
 			return elem
 
-		return elem.toprettyxml()
+		return str(elem)
 
 	@_make_elem.register('headline')
 	def _make_headline(self, node, ctx):
 		assert node.is_outline
 
-		html = self.doc.createElement('div')
-		html.attributes['class'] = 'org-header-container org-header-level-%d' % node.level
+		html = HtmlElement('div')
+		html.classes = 'org-header-container org-header-level-%d' % node.level
 
 		h_level = tag = 'h%d' % min(node.level + 1, 6)
-		header = self._make_elem_default(node, ctx, tag=tag)
-		html.appendChild(header)
+		header = self._make_elem_default(node, ctx, tag=tag, inline=True)
+		html.children.append(header)
 
 		# ID
 		if node.id:
-			html.attributes['id'] = node.id
+			html.attrs['id'] = node.id
 
 		# TODO info
 		todo_type = node.props['todo-type']
@@ -238,29 +308,31 @@ class OrgHtmlConverter:
 		if todo_type:
 			todo_kw = node.props['todo-keyword']
 
-			add_class(html, 'org-has-todo')
-			add_class(html, 'org-todo-%s' % todo_type)
-			add_class(html, 'org-todo-kw-%s' % todo_kw)
+			html.add_class('org-has-todo')
+			html.add_class('org-todo-%s' % todo_type)
+			html.add_class('org-todo-kw-%s' % todo_kw)
 
-			header.appendChild(self._make_elem_base(
+			header.children.append(self._make_elem_base(
 				'span',
 				text=todo_kw,
 				classes='org-todo org-todo-%s' % todo_type,
 			))
+			header.children.append(' ')
 
 			priority_code = node.props['priority']
 
 			if priority_code is not None:
 				priority_char = chr(priority_code)
-				header.appendChild(self._make_elem_base(
+				header.children.append(self._make_elem_base(
 					'span',
 					text=priority_char,
 					classes='org-todo-priority org-todo-priority-%s' % priority_char,
 				))
+				header.children.append(' ')
 
 		# Text
 		header_text = self.make_headline_text(node, ctx, dom=True)
-		header.appendChild(header_text)
+		header.children.append(header_text)
 
 		# Tags
 		tags = node.props['tags']
@@ -268,13 +340,14 @@ class OrgHtmlConverter:
 			tags_elem = self._make_elem_base('span', classes='org-tags')
 
 			for tag in tags:
-				tags_elem.appendChild(self._make_elem_base(
+				tags_elem.children.append(self._make_elem_base(
 					'span',
 					text=tag,
 					classes='org-tag',
 				))
 
-			header.appendChild(tags_elem)
+			header.children.append(' ')
+			header.children.append(tags_elem)
 
 		return html
 
@@ -300,7 +373,7 @@ class OrgHtmlConverter:
 
 		for item in node.contents:
 			assert item.type == 'item'
-			html.appendChild(self._convert_uo_list_item(item, ctx))
+			html.children.append(self._convert_uo_list_item(item, ctx))
 
 		return html
 
@@ -311,7 +384,7 @@ class OrgHtmlConverter:
 
 		# Checkbox state
 		if node.props['checkbox']:
-			add_class(html, 'org-checkbox org-checkbox-%s' % node.props['checkbox'])
+			html.add_class('org-checkbox org-checkbox-%s' % node.props['checkbox'])
 
 		# If first child is a paragraph, extract its contents
 		# (<p> tag inside <li> won't display correctly).
@@ -333,38 +406,38 @@ class OrgHtmlConverter:
 
 			tag = self._make_elem_base('dt')
 			self._add_children(tag, item['tag'], ctx)
-			dlist.appendChild(tag)
+			dlist.children.append(tag)
 
 			data = self._convert_node_default(item, ctx, tag='dd')
-			dlist.appendChild(data)
+			dlist.children.append(data)
 
 		return dlist
 
 	def _add_error(self, html, text=None):
 		"""Add error state to a converted HTML element."""
-		add_class(html, 'org-error')
+		html.add_class('org-error')
 		if text:
-			html.attributes['title'] = text
+			html.attrs['title'] = text
 
 	def _make_error_msg(self, msg, tag='div'):
 		return self._make_elem_base(tag, text=msg, classes='org-error-msg')
 
 	@_convert_node.register('entity')
 	def _convert_entity(self, node, ctx):
-		return self.doc.createTextNode(node['utf-8'])
+		return node['utf-8']
 
 	def _convert_link_default(self, node, ctx, url='#'):
-		html = self._make_elem_default(node, ctx)
+		html = self._make_elem_default(node, ctx, classes='org-linktype-' + node['type'])
 
 		# Add contents (these come from description part of link)
 		if node.contents:
 			self._add_children(html, node.contents, ctx)
 		else:
 			# No contents (no description), use raw-link
-			html.appendChild(self.doc.createTextNode(node['raw-link']))
-			add_class(html, 'org-link-raw')
+			html.children.append(node['raw-link'])
+			html.add_class('org-link-raw')
 
-		html.attributes['href'] = url
+		html.attrs['href'] = url
 		return html
 
 	@_convert_node.register('link')
@@ -401,16 +474,16 @@ class OrgHtmlConverter:
 		else:
 			assert False
 
-		return self.doc.createTextNode(text)
+		return text
 
 	@_convert_node.register('src-block')
 	def _convert_src_block(self, node, ctx):
 		html = self._make_elem_default(node, ctx, tag='div')
 
 		# Source code in "value" property
-		code = self._make_elem_base('pre', text=node['value'])
-		add_class(code, 'org-src-block-value')
-		html.appendChild(code)
+		code = self._make_elem_base('pre', text=node['value'], inline=True)
+		code.add_class('org-src-block-value')
+		html.children.append(code)
 
 		# The contents are the results of executing the block?
 		self._add_children(html, node.contents, ctx)
@@ -420,12 +493,12 @@ class OrgHtmlConverter:
 	@_convert_node.register('verbatim')
 	@_convert_node.register('example-block')
 	@_convert_node.register('statistics-cookie')
+	@_convert_node.register('fixed-width')
 	def _convert_node_with_value(self, node, ctx):
 		"""Convert a node with "value" property that should be its text content."""
 		value = node['value']
 		assert isinstance(value, str)
-		html = self._make_elem_default(node, ctx)
-		html.appendChild(self.doc.createTextNode(value))
+		html = self._make_elem_default(node, ctx, text=value)
 		return html
 
 	@_convert_node.register('line-break')
@@ -459,17 +532,17 @@ class OrgHtmlConverter:
 			is_head = i == 0 and len(blocks) > 1
 
 			block_elem = self._make_elem_base('thead' if is_head else 'tbody')
-			table_elem.appendChild(block_elem)
+			table_elem.children.append(block_elem)
 
 			for row in block:
 				row_elem = self._make_elem_base('tr')
-				block_elem.appendChild(row_elem)
+				block_elem.children.append(row_elem)
 
 				for cell in row:
 					assert cell.type == 'table-cell'
 
 					cell_elem = self._make_elem_base('th' if is_head else 'td')
-					row_elem.appendChild(cell_elem)
+					row_elem.children.append(cell_elem)
 
 					self._add_children(cell_elem, cell.contents, ctx)
 
@@ -480,8 +553,8 @@ class OrgHtmlConverter:
 		begin_str = node.begin.strftime(self.config['date_format'])
 
 		html = self._make_elem_default(node, ctx)
-		add_class(html, 'org-timestamp-%s' % node['type'])
-		html.appendChild(self.doc.createTextNode(begin_str))
+		html.add_class('org-timestamp-%s' % node['type'])
+		html.children.append(begin_str)
 
 		return html
 
@@ -492,12 +565,12 @@ class OrgHtmlConverter:
 		for key in ['closed', 'scheduled', 'deadline']:
 			if node[key] is not None:
 				row = self._make_elem_base('tr')
-				row.appendChild(self._make_elem_base('th', text=key.title()))
+				row.children.append(self._make_elem_base('th', text=key.title()))
 
 				td = self._make_elem_base('td')
-				td.appendChild(self._convert(node[key], ctx))
-				row.appendChild(td)
+				td.children.append(self._convert(node[key], ctx))
+				row.children.append(td)
 
-				html.appendChild(row)
+				html.children.append(row)
 
 		return html
