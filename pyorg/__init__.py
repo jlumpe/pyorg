f43b841fab791ce688bc03022bfc5a09b96664de
Jared Lumpe <mjlumpe@gmail.com>
2019-07-02 10:16:06 -0700

    Increment patch version because it actually installs now

--- pyorg/__init__.py
+++ pyorg/__init__.py
@@ -5,7 +5,7 @@ Package for woring with Emacs org-mode files
 
 __author__ = 'Jared Lumpe'
 __email__ = 'mjlumpe@gmail.com'
-__version__ = '0.1'
+__version__ = '0.1.1'
 
 from .emacs import Emacs
 from .interface import Org
