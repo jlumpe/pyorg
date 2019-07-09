2c3ae531c42087a4eff87770198de213a77ded01
Jared Lumpe <mjlumpe@gmail.com>
2019-07-08 20:16:04 -0700

    Bump version to 2.0

--- pyorg/__init__.py
+++ pyorg/__init__.py
@@ -1,11 +1,11 @@
 """Root package for pyorg.
 
-Package for woring with Emacs org-mode files
+Package for working with Emacs org-mode files
 """
 
 __author__ = 'Jared Lumpe'
 __email__ = 'mjlumpe@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 from .emacs import Emacs
 from .interface import Org
